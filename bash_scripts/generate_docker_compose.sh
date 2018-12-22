2c3313e33f68656e96a29cb9965d24741936ed76
Patrick Hudson <phudson2@gmail.com>
2018-12-21 21:48:40 -0600

    change to watcher4

--- bash_scripts/generate_docker_compose.sh
+++ bash_scripts/generate_docker_compose.sh
@@ -18,7 +18,7 @@ cat <<EOF >>docker-compose.template
       io.rancher.container.pull_image: always
     environment:
       MODELNAME: "##MODEL_NAME##"
-      SERVICE_IP: "watcher1.backend.chaturbae.tv"
+      SERVICE_IP: "watcher4.backend.chaturbae.tv"
       SYSLOG_TAG: '{docker: "myfreebae-client"}'
       LOGSTASH_FIELDS: "platform=docker,job=myfreebae-client,mfc_username=##MODEL_NAME##"
       DECODE_JSON_LOGS: "true"
