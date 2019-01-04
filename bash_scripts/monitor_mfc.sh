3cc7c25e2227a1e76b4bf06fc268514583710eae
Patrick Hudson <phudson2@gmail.com>
2019-01-03 20:08:54 -0600

    master list

--- bash_scripts/monitor_mfc.sh
+++ bash_scripts/monitor_mfc.sh
@@ -2,11 +2,11 @@
 while true
 do
   DATE=`date '+%Y-%m-%d %H:%M:%S'`
-  echo "${DATE} - scraping home page of MFC for top rooms every 5 minutes"
+  echo "${DATE} - scraping home page of MFC for top rooms every 1 minute"
   python mfc_scrape.py > temp_scrape.txt
   sort master_list.txt temp_scrape.txt | uniq > temp_scrape2.txt
   rm -rf master_list.txt
   mv temp_scrape2.txt master_list.txt
   rm -rf temp_scrape.txt
-  sleep 300
+  sleep 60
 done
