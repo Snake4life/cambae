43c2e06716d62ff796ad1ab34f725d513f3ffa88
Patrick Hudson <phudson2@gmail.com>
2019-01-04 15:28:35 -0600

    local dev work

--- cambae-backend-api/docker.sh
+++ cambae-backend-api/docker.sh
@@ -1,1 +1,1 @@
-docker build -t patt1293/cambae-backend-api:latest . && docker push patt1293/cambae-backend-api:latest
+docker build -t patt1293/cambae-backend-api:1.3 . && docker push patt1293/cambae-backend-api:1.3
