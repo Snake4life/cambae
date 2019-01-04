f0f16602a357dfcc4097e79a049bfc2f0122da3f
Patrick Hudson <phudson2@gmail.com>
2019-01-04 16:34:22 -0600

    dockerfile update

--- cambae-backend-api/docker.sh
+++ cambae-backend-api/docker.sh
@@ -1,1 +1,1 @@
-docker build -t patt1293/cambae-backend-api:1.3 . && docker push patt1293/cambae-backend-api:1.3
+docker build -t patt1293/cambae-backend-api:1.4 . && docker push patt1293/cambae-backend-api:1.4
